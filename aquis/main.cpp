79020497b3b669d4ec7ac87474e5c7a67751edc8
Constantin Dullo <constantin.dullo@gmail.com>
2017-11-11 13:55:20 +0100

    adding Xcode project

---
+++ aquis/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  aquis
+//
+//  Created by Constantin Dullo on 11.11.17.
+//  Copyright © 2017 Constantin Dullo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
